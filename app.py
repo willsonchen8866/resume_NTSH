6a66f8b36ec4f090716bbc7eed80feef25d97986
willsonchen8866 <willson12345678@g.ntsh.ntpc.edu.tw>
2025-10-08 00:22:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# 首頁
 @app.route('/')
 def index():
-    return render_template('index.html', title='讀書計畫首頁')
+    return render_template('index.html')
 
-# 各分頁路由
 @app.route('/competition')
 def competition():
-    return render_template('competition.html', title='競賽經驗')
+    return render_template('competition.html')
 
 @app.route('/activities')
 def activities():
-    return render_template('activities.html', title='課外活動')
+    return render_template('activities.html')
 
 @app.route('/leadership')
 def leadership():
-    return render_template('leadership.html', title='幹部經驗')
+    return render_template('leadership.html')
 
 @app.route('/club')
 def club():
-    return render_template('club.html', title='社團經驗')
+    return render_template('club.html')
 
 @app.route('/electives')
 def electives():
-    return render_template('electives.html', title='多元選修課程')
+    return render_template('electives.html')
 
 @app.route('/ai')
 def ai():
-    return render_template('ai.html', title='AI應用')
+    return render_template('ai.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
