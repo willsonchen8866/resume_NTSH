89644f7283b565902a2f1a4b3529957bae6fc507
pccuwang <fuhsing.wang@icloud.com>
2025-10-07 11:48:01 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/competition')
+def competition():
+    return render_template('competition.html')
+
+@app.route('/activities')
+def activities():
+    return render_template('activities.html')
+
+@app.route('/leadership')
+def leadership():
+    return render_template('leadership.html')
+
+@app.route('/club')
+def club():
+    return render_template('club.html')
+
+@app.route('/electives')
+def electives():
+    return render_template('electives.html')
+
+@app.route('/ai')
+def ai():
+    return render_template('ai.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
